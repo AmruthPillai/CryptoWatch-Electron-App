c0d4f96c32a91e31076e09c0ce8d5ada38f23c25
Amruth Pillai <im.amruth@gmail.com>
2017-12-03 18:01:06 +0530

    add notifications

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 const electron = require('electron')
 const path = require('path')
 const axios = require('axios')
+
 const BrowserWindow = electron.remote.BrowserWindow
+const ipc = electron.ipcRenderer
 
 const notifyBtn = document.getElementById('notifyBtn')
 
 var price = document.querySelector('h1')
 var targetPrice =document.getElementById('targetPrice')
+var targetPriceValue
+
+const notification = {
+  title: 'BTC Alert',
+  body: 'BTC just beat your target price!',
+  icon: path.join(__dirname, '../assets/images/btc.png')
+}
 
 function getBTC() {
   axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=INR')
     .then(res => {
       const cryptos = res.data.BTC.INR
       price.innerHTML = '₹' + cryptos.toLocaleString('hi')
+
+      if (targetPrice.innerHTML != '' && targetPriceValue < res.data.BTC.INR) {
+        const myNotification = new window.Notification(notification.title, notification)
+      }
     })
 }
 
 getBTC()
-setInterval(getBTC, 30000)
+setInterval(getBTC, 1000)
 
 notifyBtn.addEventListener('click', function (event) {
   const modalPath = path.join('file://', __dirname, 'add.html')
@@ -25,4 +38,9 @@ notifyBtn.addEventListener('click', function (event) {
   win.on('close', function () { win = null })
   win.loadURL(modalPath)
   win.show()
+})
+
+ipc.on('targetPriceValue', function (event, arg) {
+  targetPriceValue = Number(arg)
+  targetPrice.innerHTML = '₹' + targetPriceValue.toLocaleString('hi')
 })
